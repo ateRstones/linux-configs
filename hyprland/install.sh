9ea171788f82bad9cc44cf3bbfd8b74a82069988
ateRstones <8556589+ateRstones@users.noreply.github.com>
2025-05-21 19:11:54 +0200

    fix: ln sfn

--- hyprland/install.sh
+++ hyprland/install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 mkdir -p ~/.config/fish
-ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
-ln -sf $(pwd)/fish/git.fish ~/.config/fish/git.fish
+ln -sfn $(pwd)/fish/config.fish ~/.config/fish/config.fish
+ln -sfn $(pwd)/fish/git.fish ~/.config/fish/git.fish
 
 mkdir -p ~/.config/hypr
-ln -sf $(pwd)/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sf $(pwd)/hyprlock/hyprlock.conf ~/.config/hypr/hyprlock.conf
-ln -sf $(pwd)/hyprpaper/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -sfn $(pwd)/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sfn $(pwd)/hyprlock/hyprlock.conf ~/.config/hypr/hyprlock.conf
+ln -sfn $(pwd)/hyprpaper/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 
 mkdir -p ~/Documents/wallpaper
-ln -sf $(pwd)/hyprpaper/tree_fog_background.jpg ~/Documents/wallpaper/tree_fog_background.jpg
+ln -sfn $(pwd)/hyprpaper/tree_fog_background.jpg ~/Documents/wallpaper/tree_fog_background.jpg
 
 mkdir -p ~/.config/kitty
-ln -sf $(pwd)/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sfn $(pwd)/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
-ln -sf $(pwd)/starship/starship.toml ~/.config/starship.toml
+ln -sfn $(pwd)/starship/starship.toml ~/.config/starship.toml
 
 mkdir -p ~/.config/waybar
-ln -sf $(pwd)/waybar/config.jsonc ~/.config/waybar/config.jsonc
-ln -sf $(pwd)/waybar/style.css ~/.config/waybar/style.css
+ln -sfn $(pwd)/waybar/config.jsonc ~/.config/waybar/config.jsonc
+ln -sfn $(pwd)/waybar/style.css ~/.config/waybar/style.css
 
-ln -sf $(pwd)/nvim ~/.config/nvim
+ln -sfn $(pwd)/nvim ~/.config/nvim
 
-ln -sf $(pwd)/kanshi ~/.config/kanshi
+ln -sfn $(pwd)/kanshi ~/.config/kanshi
