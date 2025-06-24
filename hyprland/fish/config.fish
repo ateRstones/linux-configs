cbe6fb6fe9014e9d9b2dad782c94151a7a796234
Tom Schmolzi <tom.schmolzi@nrail.de>
2025-06-24 14:02:46 +0200

    Make sourcing fail silently if files don't exist in config.fish

--- hyprland/fish/config.fish
+++ hyprland/fish/config.fish
@@ -5,4 +5,21 @@ end
 set fish_greeting ""
 
 starship init fish | source
-source ~/.config/fish/git.fish
+if test -f ~/.config/fish/git.fish
+    source ~/.config/fish/git.fish
+end
+
+# Custom scripts etc
+set -gx SHELL_HELPERS_FOLDER "$HOME/Documents/git/shell-helpers"
+if test -f $SHELL_HELPERS_FOLDER/flux.fish
+    source $SHELL_HELPERS_FOLDER/flux.fish
+end
+if test -f $SHELL_HELPERS_FOLDER/kubectl.fish
+    source $SHELL_HELPERS_FOLDER/kubectl.fish
+end
+if test -f $SHELL_HELPERS_FOLDER/git-infrastructure.fish
+    source $SHELL_HELPERS_FOLDER/git-infrastructure.fish
+end
+if test -f $SHELL_HELPERS_FOLDER/general-helpers.fish
+    source $SHELL_HELPERS_FOLDER/general-helpers.fish
+end
